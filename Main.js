3aef67c8778716551d6f55e57796137ee6ca1d9d
andre7455 <42913113+andre7455@users.noreply.github.com>
2021-10-25 22:20:39 +0200

    updates people updates

---
+++ Main.js
@@ -0,0 +1,11 @@
+    var muziek['kaas', "kas"];
+
+    function Selector(playerId, songId){
+    //check
+    console.log(songId);
+
+    //i want to disable the button after clicking it
+    this.disabled = true;
+
+
+    }
